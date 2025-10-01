903bf7ff08ef94985e42c1c084b70a5b77700d1f
Karla Damaris Sanchez Cazares <karla.sanchez@baacc.mx>
2025-09-30 18:15:23 -0600

    01 - Se agregaron botones al centro de control

--- App.tsx
+++ App.tsx
@@ -1,13 +1,26 @@
 import "./global.css";
-import { GestureHandlerRootView} from "react-native-gesture-handler";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import Router from "@routes/RouterRoot";
 import { NavigationContainer } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
+import { cssInterop } from "nativewind";
 
-const App = function(){
+cssInterop(Ionicons, {
+  className: {
+    target: "style",
+    nativeStyleToProp: {
+      "color": "color"
+    } as Record<string, string>
+  }
+})
+
+
+const App = function () {
   return <GestureHandlerRootView>
     <NavigationContainer>
-      <Router/>
+      <Router />
     </NavigationContainer>
   </GestureHandlerRootView>
+
 }
 export default App
--- src/views/core/ControlCenter.tsx
+++ src/views/core/ControlCenter.tsx
@@ -1,13 +1,94 @@
 import { useNavigation } from "@react-navigation/native";
-import { Button, TouchableOpacity, View } from "react-native"
+import { TouchableOpacity, View, Text, ScrollView } from "react-native"
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import { Ionicons } from '@expo/vector-icons';
+import { gs } from "@/src/utils/styles";
 
-const ControlCenter = function () {
-    const go = useNavigation()
-    return <View>
-        <Button title="Navegar al Calendario" onPress={function () { 
-            go.navigate('Calendar') 
-        }}>
-        </Button>
-    </View>
-}
+const ControlCenter = () => {
+    const go = useNavigation();
+    return (
+
+        <View 
+        className="bg-[#99d8f3] dark:bg-[#000000]"
+        style={{ flex: 1, padding: 12,}}
+        >
+            <ScrollView contentContainerStyle={gs.scroll}>
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        flexWrap: 'wrap',
+                        justifyContent: 'space-between',
+                    }}>
+                    
+                    <TouchableOpacity
+                        onPress={() => go.navigate('Calendar')}
+                        style={{
+                            width: '49%',
+                            aspectRatio: 1,
+                            backgroundColor: '#0078D7',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            marginVertical:'1%',
+                            marginBottom: 12,
+                            borderRadius: 8,
+                        }}>
+                        <Ionicons name="calendar" size={50} className="text-white dark:text-black" />
+                        <Text style={{ color: '#fff' , fontSize: 18, fontWeight: 'bold' }}>
+                            Calendario
+                        </Text>
+                        <Text style={{ fontSize: 14, textAlign: 'center', color: '#eee' }}>
+                            Ver eventos
+                        </Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity
+                        onPress={() => go.navigate('Documents')}
+                        style={{
+                            width: '49%', 
+                            aspectRatio: 1,
+                            backgroundColor: '#FF8C00',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            marginVertical:'1%',
+                            marginBottom: 12,
+                            borderRadius: 8,
+                        }}>
+                        <Ionicons name="documents" size={50} className="text-white dark:text-black" />
+                        <Text style={{ color: '#fff', fontSize: 18, fontWeight: 'bold' }}>
+                            Documentos
+                        </Text>
+                        <Text style={{ fontSize: 14, textAlign: 'center', color: '#eee' }}>
+                            Inspeccionar
+                        </Text>
+
+                    </TouchableOpacity>
+
+                    <TouchableOpacity
+                        onPress={() => go.navigate('VR')}
+                        style={{
+                            width: '49%',
+                            aspectRatio: 1,
+                            backgroundColor: '#008f39',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            marginVertical:'1%',
+                            marginBottom: 12,
+                            borderRadius: 8,
+                        }}>
+                        <MaterialCommunityIcons name="virtual-reality" size={60}  />
+                        <Text style={{ color: '#fff', fontSize: 18, fontWeight: 'bold' }}>
+                            Variables
+                        </Text>
+                        <Text style={{ fontSize: 14, textAlign: 'center', color: '#eee' }}>
+                            Inspeccionar
+                        </Text>
+
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+        </View>
+    );
+};
+
+
 export default ControlCenter;
